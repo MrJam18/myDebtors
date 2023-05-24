5c810539a14c3bf903023732b9efa0669240431c
jamil <mr.jam18@yandex.ru>
2023-05-24 16:29:02 +0400

    registration added

---
+++ resources/js/hooks/useForwardRef.ts
@@ -0,0 +1,3 @@
+export function useForwardRef() {
+    $END$
+}
---
+++ resources/js/store/Dispatchers/Auth/RegistrationDispatcher.ts
@@ -0,0 +1,9 @@
+import {Dispatcher} from "../Abstract/Dispatcher";
+
+export class RegistrationDispatcher extends Dispatcher
+{
+    protected async _handler(dispatcherData)
+    {
+        
+    }
+}

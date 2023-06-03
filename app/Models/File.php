785c8f96f83c1477a0c55c08d2df46e8a899349d
yagoist <yagogo83@gmail.com>
2023-06-03 13:11:10 +0300

    add file and comment components

---
+++ app/Models/File.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Base\BaseModel;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id;
+ * @property string $url;
+ * @property Carbon $created_at;
+ * @property Carbon $updated_at;
+ */
+
+class File extends BaseModel
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'url',
+    ];
+
+    public $timestamps = true;
+}
